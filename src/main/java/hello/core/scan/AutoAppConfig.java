fddee77e5fba581c560f630f642e617a57e11774
deokmoon <deok2moon@gmail.com>
2023-10-16 20:45:26 +0900

    [FEAT]SECTION7

--- src/main/java/hello/core/scan/AutoAppConfig.java
+++ src/main/java/hello/core/scan/AutoAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member", // default 해당 Config 가 속한 위치부터 하위
+        basePackages = "hello.core.*", // default 해당 Config 가 속한 위치부터 하위
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // 해당 Config 가 잘 동작하는지 확인하기 위해서는 기존 AppConfig 등 다른 Configuration에 수기로 등록한 빈들을 제외 필요
 public class AutoAppConfig {
